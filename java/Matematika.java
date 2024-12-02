f28173e098415d2c234bcefc16703747ff183d16
Mektildisadvenia <186996952+Mektildisadvenia@users.noreply.github.com>
2024-12-02 15:38:08 +0700

    Add files via upload

---
+++ java/Matematika.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asahi
+ */
+public class Matematika {
+      int mtk;
+            void pertambahan(int a, int b){
+                mtk = a + b;
+            }
+            void perkalian(int a, int b){
+                mtk = a * b;
+            }
+}
+       
+
+
---
+++ java/MatematikaCanggihBeraksi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asahi
+ */
+public class MatematikaCanggihBeraksi {
+  public static void main(String[] args){
+        MatematikaCanggih call = new MatematikaCanggih();
+        call.pertambahan(7,3);
+        System.out.println("pertambahan 7 + 3 =" + call.mtk);
+        call.perkalian(3, 3);
+        System.out.println("perkalian 3 * 3=" + call.mtk);
+        call.modulus(5 , 2);
+        System.out.println("modulus 5 * 2 =" + call.mtk);
+  }
+}
+
